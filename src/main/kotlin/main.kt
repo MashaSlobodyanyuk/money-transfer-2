28c59a17a76d4156cc94b99724e87321aa8ad7ac
Masha S <mashaslobodyanyuk@gmail.com>
2023-06-12 22:28:10 +0300

    Correct

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,35 +1,46 @@
-fun main(){
-    limit("MasterCard", 200_000_00.0, 0)
+fun main() {
+    limit(148_000_99, "MasterCard", 0)
+    limit(2_570_00)
 }
 
 
-fun limit(cart: String, transfer: Double, transferLast: Int) {
+fun limit(transfer: Int, cart: String = "VK Pay", transferLast: Int = 0) {
     when (cart) {
         "MasterCard", "Maestro", "Visa", "Mir" ->
-            if ((transferLast + transfer) > 600_000_00)
+            if ((transferLast + transfer) > 600_000_00) {
                 println("Превышен месячный лимит на перевод через банковскую карту")
-            else if (transfer <= 150_000_00) card(cart, transfer, transferLast)
-            else println("Превышен суточный лимит на перевод через банковскую карту")
-
-        else -> if ((transferLast + transfer) > 40_000_00)
+            } else if (transfer <= 150_000_00) {
+                var summCommision = card(transfer, cart, transferLast)
+                println("Комиссия составит  ${String.format("%.2f", summCommision)} рублей")
+            } else {
+                println("Превышен суточный лимит на перевод через банковскую карту")
+            }
+        else -> if ((transferLast + transfer) > 40_000_00) {
             println("Превышен месячный лимит на перевод через VK Pay")
-        else if (transfer <= 15_000_00) card(cart, transfer, transferLast)
-        else println("Превышен суточный лимит на перевод через VK Pay")
+        } else if (transfer <= 15_000_00) {
+            var summCommision = card(transfer, cart, transferLast)
+            println("Комиссия составит ${String.format("%.2f", summCommision)} рублей")
+        } else println("Превышен суточный лимит на перевод через VK Pay")
 
     }
 }
 
-fun card(cart: String, transfer: Double, transferLast: Int) {
+fun card(transfer: Int, cart: String = "VK Pay", transferLast: Int = 0): Double {
     when (cart) {
-        "MasterCard", "Maestro" -> if (transferLast >= 300_00 && transferLast <= 75000_00)
-            println("Комиссия составит 0 рублей")
-        else
-            println("Комиссия составит ${(transfer * 0.006 + 20_00) / 100} рублей")
-        "Visa", "Mir" -> if (transferLast <= 4666_67)
-            println("Комиссия составит 35 рублей")
-        else
-            println("Комиссия составит ${(transfer * 0.0075) / 100}рублей")
-        else -> println("Комиссия составит 0 рублей")
+        "MasterCard", "Maestro" ->
+            if (transferLast + transfer >= 300_00 && transferLast + transfer <= 75000_00) {
+                return 0.0
+            } else {
+                var summCommision = (transfer * 0.006 + 20_00) / 100
+                return summCommision
+            }
+        "Visa", "Mir" -> if (transferLast <= 4666_67) {
+            return 35.0
+        } else {
+            var summCommision = (transfer * 0.0075) / 100
+            return summCommision
+        }
+        else -> return 0.0
     }
 
 }
