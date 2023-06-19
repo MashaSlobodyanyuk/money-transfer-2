9882865e88586b9ad5eb08cf30c3df802a4858fe
Masha S <mashaslobodyanyuk@gmail.com>
2023-06-19 18:31:04 +0300

    aftotest

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,26 +1,26 @@
 fun main() {
-    limit(148_000_99, "MasterCard", 0)
-    limit(2_570_00)
+    println(limit(148_000_99, "MasterCard", 0))
+    println(limit(2_570_00))
 }
 
 
-fun limit(transfer: Int, cart: String = "VK Pay", transferLast: Int = 0) {
+fun limit(transfer: Int, cart: String = "VK Pay", transferLast: Int = 0): String {
     when (cart) {
         "MasterCard", "Maestro", "Visa", "Mir" ->
             if ((transferLast + transfer) > 600_000_00) {
-                println("Превышен месячный лимит на перевод через банковскую карту")
+                return "Превышен месячный лимит на перевод через банковскую карту"
             } else if (transfer <= 150_000_00) {
-                var summCommision = card(transfer, cart, transferLast)
-                println("Комиссия составит  ${String.format("%.2f", summCommision)} рублей")
+                val summCommision = card(transfer, cart, transferLast)
+                return "Комиссия составит  ${String.format("%.2f", summCommision)} рублей"
             } else {
-                println("Превышен суточный лимит на перевод через банковскую карту")
+                return "Превышен суточный лимит на перевод через банковскую карту"
             }
         else -> if ((transferLast + transfer) > 40_000_00) {
-            println("Превышен месячный лимит на перевод через VK Pay")
+            return "Превышен месячный лимит на перевод через VK Pay"
         } else if (transfer <= 15_000_00) {
-            var summCommision = card(transfer, cart, transferLast)
-            println("Комиссия составит ${String.format("%.2f", summCommision)} рублей")
-        } else println("Превышен суточный лимит на перевод через VK Pay")
+            val summCommision = card(transfer, cart, transferLast)
+            return "Комиссия составит ${String.format("%.2f", summCommision)} рублей"
+        } else return "Превышен суточный лимит на перевод через VK Pay"
 
     }
 }
@@ -31,13 +31,13 @@ fun card(transfer: Int, cart: String = "VK Pay", transferLast: Int = 0): Double 
             if (transferLast + transfer >= 300_00 && transferLast + transfer <= 75000_00) {
                 return 0.0
             } else {
-                var summCommision = (transfer * 0.006 + 20_00) / 100
+                val summCommision = (transfer * 0.006 + 20_00) / 100
                 return summCommision
             }
-        "Visa", "Mir" -> if (transferLast <= 4666_67) {
+        "Visa", "Mir" -> if (transfer <= 4666_67) {
             return 35.0
         } else {
-            var summCommision = (transfer * 0.0075) / 100
+            val summCommision = (transfer * 0.0075) / 100
             return summCommision
         }
         else -> return 0.0
